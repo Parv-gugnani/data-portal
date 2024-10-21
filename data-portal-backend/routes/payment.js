3b46512d575fa7adf4418617aab9864d3932a8c5
Parv-gugnani <9765.parv@gmail.com>
2024-10-21 13:22:28 +0530

    starting

--- data-portal-backend/routes/payment.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { buyCredits } = require("../controllers/paymentController");
-const router = express.Router();
-
-router.post("/buy-credits", buyCredits);
-
-module.exports = router;
